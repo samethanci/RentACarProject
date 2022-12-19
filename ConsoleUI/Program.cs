8a58571a13c39758cfa6695de31dcb9dd957723e
Samet HANCI <Samet HANCI@SAMET>
2022-12-19 14:52:51 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            carManager.Add(new Car { ColorId = 2, DailyPrice = 1591.05M, Description = "Mercedes C Serisi", ModelYear = "2019" });
+            carManager.Add(new Car { ColorId = 3, DailyPrice = 1647.8M, Description = "Volvo XC40", ModelYear = "2020" });
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/ProjectDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class ProjectDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"(localdb)\mssqllocaldb;Database=ReCapProjectDB;trusted_connection=true");
+        }
+        public DbSet<Car> Tbl_Cars;
+        public DbSet<Color> Tbl_Color;
+        public DbSet<Brand> Tbl_Brand;
+    }
+}
