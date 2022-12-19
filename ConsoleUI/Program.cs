9230273c02df943f94ba98c52c1297af8ad25e32
Samet HANCI <Samet HANCI@SAMET>
2022-12-19 17:01:49 +0300

    19/12/2022

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleUI
             CarManager carManager = new CarManager(new EfCarDal());
             //carManager.GetCarsByBrandId(3);
             //carManager.Add(new Car { BrandID = 2, ColorId = 1, ModelYear = "2022", DailyPrice = 1500, Description = "2022 Model" });
-            carManager.Add(new Car { BrandID = 9, ColorId = 5, ModelYear = "2000", DailyPrice = 300, Description = "2000 Model" });
+            carManager.Add(new Car {BrandID = 6, ColorId = 1, ModelYear = "2010", DailyPrice = 1300, Description = "2010 Model Ford" });
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Description);
